7c3277f0eb77bdda8fc9b405604545d790925f0d
elicha1 <aicha95bouichou@gmail.com>
2023-08-11 04:54:22 -0700

    fisrt

---
+++ README.md
@@ -0,0 +1,1 @@
+readme simple_shell
---
+++ simplecode.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int x = 10;
+    int y = 20;
+
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else
+    {
+        printf("x is not less than y\n");
+    }
+
+    return 0;
+}
